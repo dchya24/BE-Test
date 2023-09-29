bce6141b449d05f87b0ab934062040fbd9cc3436
dchya24 <cahyadinar241@gmail.com>
2023-09-29 11:32:10 +0700

    Add sql file

---
+++ files/user_backup.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO public.users (digits,"fotoUrl","workType","positionTitle",lat,lon,company,"isLogin","createdAt","updatedAt",dovote,dosurvey,dofeedback,fullname,"cuurentLeave") VALUES
+	 ('DFA','','WFO','manager',0.0,0.0,'NTX',true,'2021-12-15 15:06:33+07','2022-12-22 21:05:32.377+07',true,true,false,'M. Daffa Quraisy',0),
+	 ('HFW','','WFO','hrd',0.0,0.0,'NTX',true,'2021-12-15 15:06:33.226343+07','2022-12-22 15:03:46.848+07',true,false,false,'Hafidz Wibowo',0),
+	 ('HTA','','WFH','pegawai',0.0,0.0,'NTX',true,'2021-12-15 15:06:33+07','2023-09-26 22:41:31.248658+07',true,true,false,'R. Hernanta Subagya',0);
